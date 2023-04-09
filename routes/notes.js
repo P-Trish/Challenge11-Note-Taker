be79393f407860518578c3899b68f3dd7000596b
Patricia <124640646+P-Trish@users.noreply.github.com>
2023-04-09 13:54:12 -0700

    added GET notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,20 @@
+// Import all required packages
+const notes = require ('express').Router();
+const {readFromFile, readAndAppend, writeToFile} = require('../helpers/fsUtils');
+const uuid = require('../utils/uuid');
+
+// GET route 
+
+
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json')
+    .then((data) => res.json(JSON.parse(data)));
+});
+
+
+// POST route
+
+
 
 
 

9b53f3afd3328d9b15406ad273c59ae2bc90780e
Patricia <124640646+P-Trish@users.noreply.github.com>
2023-04-09 13:39:16 -0700

    populated index.js

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,11 @@
+// import all required modules
 
+const express = require('express');
 
+const app = express();
 
+const notesRouter = require('./routes/notes');
 
+app.use('/notes', notesRouter);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// module.exports = () { }
+module.exports = app;
